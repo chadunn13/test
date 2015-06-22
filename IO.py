6d2162351b33f516028be072be01457f66814adb
chadunn13 <chadunn13@gmail.com>
2015-06-22 16:10:51 -0400

    Update IO.py

--- IO.py
+++ IO.py
@@ -1,1 +1,13 @@
 import pyaudio
+
+import logging
+import tempfile
+import wave
+import audioop
+import alteration
+import jasperpath
+
+class IO:
+  
+  def __init__(self, speaker, passive_stt_engine, active_stt_engine):
+    self._logger = logging.getLogger(__name__)
