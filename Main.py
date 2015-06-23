4236b39ff712804a68139f3418751f625174af64
chadunn13 <chadunn13@gmail.com>
2015-06-23 11:21:45 -0400

    Update and rename Main.py to main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import parser
-
-def main():
-  parser.parse(input("give a command"))
-  
-if __name__ == "__main__":
-  main()
---
+++ main.py
@@ -0,0 +1,8 @@
+import datetime
+from parser.py import Parser
+
+def main():
+  Parser.parseInput(input("give a command"))
+  
+if __name__ == "__main__":
+  main()
