a1d6bfcf4561708a0dd637797840b65af821df1f
chadunn13 <chadunn13@gmail.com>
2015-06-23 11:20:55 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ from io.py import IO
 from datetime_files.datetimecommands import DateTimeCommands
 from datetime_files.datetime2 import DateTime2
 
-class parser:
+class Parser:
   
   def parseInput(string_input):
     if string_input in DateTimeCommands.time_now_commands:
