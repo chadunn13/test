30ff6fed9eeb6acd24bf2ba03349bcc35a390d0c
chadunn13 <chadunn13@gmail.com>
2015-06-23 11:15:34 -0400

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,9 @@
+from io.py import IO
+from datetime_files.datetimecommands import DateTimeCommands
+from datetime_files.datetime import 
+
+class parser:
+  
+  def parseInput(string_input):
+    if string_input in DateTimeCommands.timenow_commands:
+      
