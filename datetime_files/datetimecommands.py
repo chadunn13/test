bc645a296cc795ede557e824806870fca7bf1af1
chadunn13 <chadunn13@gmail.com>
2015-06-23 15:08:43 -0400

    Update datetimecommands.py

--- datetime_files/datetimecommands.py
+++ datetime_files/datetimecommands.py
@@ -1,8 +1,5 @@
 class DateTimeCommands:
   
-  time_now_commands = "what time is it", "what time's it", "what's the time", "what is the time", "can you tell me the time", +
-    "time check", "check time", "check the time", "what time is it right now", "what time's it now", "what time's it right now", +
-    "what's the time now", "what's the time right now", "what time is it now", "what is the time now", "what is the time right now"
+  time_now_commands = "what time is it", "what time's it", "what's the time", "what is the time", "can you tell me the time", "time check", "check time", "check the time", "what time is it right now", "what time's it now", "what time's it right now", "what's the time now", "what's the time right now", "what time is it now", "what is the time now", "what is the time right now"
   
-  date_today_commands = "what is today", "what's today", "what is the date", "what's the date", "what is the date today", +
-    "what's the date today", "what day is it", "what day's it", "what date is it", "what date's it"
+  date_today_commands = "what is today", "what's today", "what is the date", "what's the date", "what is the date today", "what's the date today", "what day is it", "what day's it", "what date is it", "what date's it"
