3d16f617d77021da0a17c23894fab65dfd981bb9
chadunn13 <chadunn13@gmail.com>
2015-06-23 10:52:42 -0400

    Update DateTime.py

--- DateTime.py
+++ DateTime.py
@@ -9,5 +9,27 @@ def getDateToday():
   month_say = month_tuple[month_num]
   day_say = date_today.day
   year_say = date_today.year
-  date_say = "Today is " + month_say + " " + str(day_say) + ", " + (year_say) + ", " + (master.name)
+  date_say = "Today is " + month_say + " " + str(day_say) + ", " + str(year_say) + ", " + master.name
   IO.say(date_say)
+
+def getTimeNow():
+  time_now = datetime.time.time()
+  hour_num = time_now.hour
+  hour_say = 0
+  am_pm_say = ""
+  
+  if hour_num == 0:
+    hour_say = 12
+    am_pm_say = "AM"
+  elif hour_num < 12:
+    hour_say = hour_num
+    am_pm_say = "AM"
+  elif hour_num == 12:
+    hour_say = 12
+    am_pm_say = "PM"
+  else:
+    hour_say = str(hour_num - 12)
+    am_pm_say = "PM"
+    
+  minute_say = time_now.minutes
+  time_say = "The time is" + hour_say + " " + minute_say + " " + am_pm_say
