01b7233299d6508d4037174102c62ccd5dbcc11b
chadunn13 <chadunn13@gmail.com>
2015-06-23 09:13:04 -0400

    Update DateTime.py

--- DateTime.py
+++ DateTime.py
@@ -1,2 +1,13 @@
 import datetime
+import master
+import IO
 
+def getDateToday():
+  month_tuple = "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+  date_today = datetime.date.today()
+  month_num = date_today.month
+  month_say = month_tuple(month_num)
+  day_say = date_today.day
+  year_say = date_today.year
+  date_say = "Today is " + month_say + " " + day_say + ", " + year_say + ", " + master.name
+  IO.say(date_say)
