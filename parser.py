249834eb379f8e8df714f206b2f26dd513f2ccb3
chadunn13 <chadunn13@gmail.com>
2015-06-23 11:59:22 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
-class Parser:
-  
-  def parseInput(string_input):
-    if string_input in DateTimeCommands.time_now_commands:
-      DateTime2.getTimeNow()
-    elif string_input in DateTimeCommands.date_today_commands:
-      DateTime2.getDateToday()
+
+def parseInput(string_input):
+  if string_input in DateTimeCommands.time_now_commands:
+    DateTime2.getTimeNow()
+  elif string_input in DateTimeCommands.date_today_commands:
+    DateTime2.getDateToday()
