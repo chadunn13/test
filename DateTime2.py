4c3f04bdbc2ff80aa932d6767068db65bc457ed7
chadunn13 <chadunn13@gmail.com>
2015-06-23 09:38:09 -0400

    Create DateTime2.py

---
+++ DateTime2.py
@@ -0,0 +1,17 @@
+import datetime
+
+def main():
+	getDateToday()
+
+def getDateToday():
+  month_tuple = "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+  date_today = datetime.date.today()
+  month_num = date_today.month
+  month_say = month_tuple[month_num]
+  day_say = date_today.day
+  year_say = date_today.year
+  date_say = "Today is " + month_say + " " + day_say + ", " + year_say + ", Master"
+  print(date_say)
+  
+if __name__ == "__main__":
+	main()# your code goes here
